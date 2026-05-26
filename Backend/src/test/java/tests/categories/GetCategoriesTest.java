b9b5786ac4ded450971f769edd24c528a7c1f06a
adiiaa <adia.uwase24snhu@keplercollege.ac.rw>
2026-05-26 16:17:24 +0200

    test: add getAllCategories test scenarios

--- Backend/src/test/java/tests/categories/GetCategoriesTest.java
+++ Backend/src/test/java/tests/categories/GetCategoriesTest.java
@@ -4,8 +4,7 @@ import Base.ThinkGetItAPI;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.*;
 
 public class GetCategoriesTest {
     ThinkGetItAPI api = new ThinkGetItAPI();
@@ -14,5 +13,16 @@ public class GetCategoriesTest {
     void testGeTAllCategories(){
         Response response = api.getCategories();
         assertEquals(response.statusCode(), 200);
+        assertNotNull(response.jsonPath().get("data"));
+        int size = response.jsonPath().getList("data").size();
+        assertTrue(size>0);
+
+        String firstName = response.jsonPath().getString("data[0].name");
+        System.out.println("First category name: " + firstName);
+        assertNotNull(firstName);
+
+        String firstSlug = response.jsonPath().getString("data[0].slug");
+        System.out.println("First category slug: " + firstSlug);
+        assertNotNull(firstSlug);
     }
 }
