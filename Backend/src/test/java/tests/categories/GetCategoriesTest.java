aac0a8089f17dfa667f886605d6a4c2da99c16c0
adiiaa <adia.uwase24snhu@keplercollege.ac.rw>
2026-05-26 14:53:18 +0200

    added some tests

--- Backend/src/test/java/tests/categories/GetCategoriesTest.java
+++ Backend/src/test/java/tests/categories/GetCategoriesTest.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 public class GetCategoriesTest {
     ThinkGetItAPI api = new ThinkGetItAPI();
