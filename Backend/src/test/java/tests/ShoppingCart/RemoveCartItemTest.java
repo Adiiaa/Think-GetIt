b365fbdaeeb8e058efa860c785be1b762ff52a56
adiiaa <adia.uwase24snhu@keplercollege.ac.rw>
2026-06-02 10:30:01 +0200

    test: implementing remove cart item with invalid item id API test scenario

--- Backend/src/test/java/tests/ShoppingCart/RemoveCartItemTest.java
+++ Backend/src/test/java/tests/ShoppingCart/RemoveCartItemTest.java
@@ -29,4 +29,9 @@ public class RemoveCartItemTest {
             Response response = api.removeCartItem(cartItemId, token);
             assertEquals(response.statusCode(), 200);
     }
+    @Test
+    void testRemoveCartItemWithInvalidId(){
+        Response response = api.removeCartItem("invalidId", token);
+        assertEquals(response.statusCode(), 404);
+    }
 }
