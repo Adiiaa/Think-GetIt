41b4867910bd6b242649c2c0f451e8109ec211e5
adiiaa <adia.uwase24snhu@keplercollege.ac.rw>
2026-06-05 10:39:19 +0200

    test: testing if new user has empty withlist

--- Backend/src/test/java/tests/Reviews/GetWishlistTest.java
+++ Backend/src/test/java/tests/Reviews/GetWishlistTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import util.TestUserFactory;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 public class GetWishlistTest {
 
@@ -21,10 +22,18 @@ public class GetWishlistTest {
     void testGetWishlistWithValidToken(){
         Response response = api.getWishlist(token);
         assertEquals(response.statusCode(), 200);
+        assertNotNull(response.jsonPath().get("data"));
     }
     @Test
     void testGetWithlistWithNoToken(){
         Response response = api.getWishlist("");
         assertEquals(response.statusCode(), 401);
+    }
+    @Test
+    void testNewUserHasEmptyWishlist(){
+        Response response = api.getWishlist(token);
+        assertEquals(response.statusCode(), 200);
+        int size = response.jsonPath().getList("data").size();
+        assertEquals(size, 0);
     }
 }
